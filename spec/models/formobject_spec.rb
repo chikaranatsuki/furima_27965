fe0574b834a171ddc4d638c2c47cdae195e02b56
chikaranatsuki <69283318+chikaranatsuki@users.noreply.github.com>
2021-01-11 12:22:01 +0900

    formobjectの単体テスト

--- spec/models/formobject_spec.rb
+++ spec/models/formobject_spec.rb
@@ -12,28 +12,55 @@ RSpec.describe PayForm, type: :model do
   describe '商品購入' do
     context '内容に問題ない場合' do
       it '全て正常' do
+        expect(@pay_form.valid?).to eq true
       end
     end
-    context "内容に問題がある場合" do
-      it "tokenがない" do
+   
+    context '内容に問題がある場合' do
+      it 'token:必須' do
+        @pay_form.token = ''
+        @pay_form.valid?
+        expect(@pay_form.errors.full_messages).to include("Token can't be blank")
       end
-      it "郵便番号が空欄" do
+      it 'postal_code:必須' do
+        @pay_form.postal_code = ''
+        @pay_form.valid?
+        expect(@pay_form.errors.full_messages).to include("Postal code can't be blank")
       end
-      it "郵便番号のフォーマットが正しくない" do
+      it 'postal_code:フォーマット' do
+        @pay_form.postal_code = '1234567'
+        @pay_form.valid?
+        expect(@pay_form.errors.full_messages).to include("Postal code Input correctly")
       end
-      it "都道府県が空欄" do
+      it 'prefecture:必須' do
+        @pay_form.prefecture = nil
+        @pay_form.valid?
+        expect(@pay_form.errors.full_messages).to include("Prefecture can't be blank")
       end
-      it "都道府県の選択をしていない" do
+      it 'prefecture:0以外' do
+        @pay_form.prefecture = 0
+        @pay_form.valid?
+        expect(@pay_form.errors.full_messages).to include("Prefecture Select")
       end
-      it "市区町村GA空欄" do
+      it 'city:必須' do
+        @pay_form.city = ''
+        @pay_form.valid?
+        expect(@pay_form.errors.full_messages).to include("City can't be blank")
       end
-      it "番地が空欄" do
+      it 'addresses:必須' do
+        @pay_form.addresses = ''
+        @pay_form.valid?
+        expect(@pay_form.errors.full_messages).to include("Addresses can't be blank")
       end
-      it "電話番号が空欄" do
+      it 'phone_number:必須' do
+        @pay_form.phone_number = ''
+        @pay_form.valid?
+        expect(@pay_form.errors.full_messages).to include("Phone number can't be blank")
       end
-      it "電話番号が11桁以内" do
-      end
-      it "電話番号が数字のみか" do
+      it 'phone_number:11桁以内' do
+        @pay_form.phone_number = '1234567891011'
+        @pay_form.valid?
+        expect(@pay_form.errors.full_messages).to include("Phone number Too long")
       end
     end
   end
